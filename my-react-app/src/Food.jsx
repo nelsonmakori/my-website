f8fc105d608abbf037a33a04e6ab165ca61e94f4
nelsonmakori <nelsonmakori6@gmail.com>
2024-09-10 16:22:34 -0600

    Initial commit

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,17 @@
+function Food(){
+
+  const food1 ="orange";
+  const food2 = 'banana';
+  const food3 = 'strawbelly';
+
+
+  return(
+    <ul>
+      <li>apple</li>
+      <li>avocado</li>
+      <li>{food3}</li>
+    </ul>
+  );
+
+}
+export  default Food
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+  return(
+    <header>
+      <h1>my wesbite</h1>
+      <nav className="nav">
+        <ul>
+          <li><a href="#"></a>Home</li>
+          <li><a href="#"></a>About</li>
+          <li><a href="#"></a>services</li>
+          <li><a href="#"></a>contacts</li>
+        </ul>
+      </nav>
+      <hr></hr>
+    </header>
+  );
+
+}
+export default Header
